092089033f8ae798f83ad62ad7df97346e0bb682
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-11-12 04:45:46 -0800

    the emails are now styled

--- src/server/twilio/sendEmail.ts
+++ src/server/twilio/sendEmail.ts
@@ -1,11 +1,34 @@
+import { string } from 'zod'
 import sgMail from './sendgrid'
 
-const sendEmail = (to_mail: string, from_mail: string) => {
+
+interface Email {
+  from_mail: string;
+  to_mail: string;
+  raza: string;
+  imagen: string;
+  color?: string;
+  detalles?: string;
+  edad?: number;
+}
+
+const sendEmail = (email: Email) => {
   let msg = {
-    to: from_mail,
-    from: to_mail,
+    from: email.from_mail,
+    to: email.to_mail,
     subject: 'Acerca de tu cuenta',
-    html: 'Se ha creado tu evento '
+    html: `
+      <div style="font-family: inherit; text-align: inherit"><b>Hola. Queremos informarte que un perro se ha extraviado cerca de tí. El perro tiene las siguientes características: </b>&nbsp;</div>
+      <div style="font-family: inherit; text-align: inherit">· <b>Raza</b>: ${email.raza}. &nbsp;</div>
+      ${email.color ? `<div style="font-family: inherit; text-align: inherit">&emsp;· <b>Color</b>: ${email.color}. &nbsp;</div>` : ''}
+      ${email.edad ? `<div style="font-family: inherit; text-align: inherit">&emsp;· <b>Edad</b>: ${email.edad}. &nbsp;</div>` : ''}
+      ${email.detalles ? `<div style="font-family: inherit; text-align: inherit">&emsp;· <b>Detalles</b>: ${email.detalles}. &nbsp;</div>` : ''}
+      <div style="font-family: inherit; text-align: inherit"><br /></div>
+      <div style="font-family: inherit; text-align: inherit">Se muestra una foto del perro extraviado a continuación:</div>
+      <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+        <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:100% !important; width:100%; height:auto !important;" width="0" alt="" data-proportionally-constrained="true" data-responsive="true" src="${email.imagen}">
+      </td>
+    `
   }
   sgMail
     .send(msg)
