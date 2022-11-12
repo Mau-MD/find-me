026bf034ff72b0eac0abb58c3bc2e7273ae6afbf
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-11-12 06:16:34 -0800

    stepped

---
+++ src/components/StepsToSaveAPet/Stepper.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { Stepper, Button, Group } from '@mantine/core';
+
+function Steps() {
+    const [active, setActive] = useState(0);
+    const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
+    const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+
+    return (
+        <>
+            <Stepper active={active} onStepClick={setActive} breakpoint="sm">
+                <Stepper.Step label="First step" description="Create an account" allowStepSelect={active > 0}>
+                    Step 1 content: Create an account
+                </Stepper.Step>
+                <Stepper.Step label="Second step" description="Verify email" allowStepSelect={active > 1}>
+                    Step 2 content: Verify email
+                </Stepper.Step>
+                <Stepper.Step label="Final step" description="Get full access" allowStepSelect={active > 2}>
+                    Step 3 content: Get full access
+                </Stepper.Step>
+                <Stepper.Step label="Final step" description="Get full access" allowStepSelect={active > 3}>
+                    Step 3 content: Get full access
+                </Stepper.Step>
+                <Stepper.Completed>
+                Completed, click back button to get to previous step
+                </Stepper.Completed>
+            </Stepper>
+
+            <Group position="center" mt="xl">
+                <Button variant="default" onClick={prevStep}>Back</Button>
+                <Button onClick={nextStep}>Next step</Button>
+            </Group>
+        </>
+    );
+}
+export default Steps;
