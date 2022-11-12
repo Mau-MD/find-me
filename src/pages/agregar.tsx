82794485368152611fdbe446e37378b28db7204a
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-11-12 01:14:09 -0800

    firebase upload images

--- src/pages/agregar.tsx
+++ src/pages/agregar.tsx
@@ -1,14 +1,27 @@
 
 import { type NextPage } from "next";
-import { Group, Text, Flex, TextInput, Textarea, NumberInput, useMantineTheme, Checkbox, Select } from '@mantine/core'
-import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from '@mantine/dropzone';
-import { useRef } from "react";
+import { Group, Text, Flex, TextInput, Textarea, NumberInput, useMantineTheme, Checkbox, Select, Button } from '@mantine/core'
+import { Dropzone, DropzoneProps, FileWithPath, IMAGE_MIME_TYPE } from '@mantine/dropzone';
+import { useState, useRef } from "react";
 import { IconUpload, IconPhoto, IconX } from '@tabler/icons';
+import { storage } from '../firebase/firebase'
+import { ref, uploadBytes } from 'firebase/storage'
+import { v4 } from 'uuid'
 
 
 const agregar: NextPage = () => {
+  const [imageUpload, setImageUpload] = useState<FileWithPath | null>(null);
   const openRef = useRef<() => void>(null);
   const theme = useMantineTheme();
+
+  const uploadImage = () => {
+    if (imageUpload == null) return
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload)
+      .then(() => {
+        console.log("Image uploaded")
+      })
+  }
 
   return (
     <div>
@@ -53,7 +66,11 @@ const agregar: NextPage = () => {
       />
 
       <Dropzone
-        onDrop={(files) => console.log('accepted files', files)}
+        onDrop={(files) => {
+          if (files[0] == null || files[0] == undefined) return; 
+          setImageUpload(files[0])
+          console.log('done')
+        }}
         onReject={(files) => console.log('rejected files', files)}
         maxSize={3 * 1024 ** 2}
         accept={IMAGE_MIME_TYPE}
@@ -86,6 +103,10 @@ const agregar: NextPage = () => {
           </div>
         </Group>
       </Dropzone>
+
+      <Button onClick={uploadImage}>
+        Click here
+      </Button>
     </div>
   )
 }
