7f35333e875eae8e1a475075f937e1e545c98f73
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-11-12 14:03:55 -0800

    soyBruno2
    si

--- src/utils/get-stripe.ts
+++ src/utils/get-stripe.ts
@@ -8,10 +8,10 @@ export async function checkout({
   let stripePromise: Promise<Stripe | null>;
 
   const getStripe = () => {
+    const stripe_publishable_key =
+      "pk_test_51M3MArCySmvqtgpSH7FdAL8TOb16Nscz3oUde8cLGocd0QOU8QCSl4io9WXWv5lcPxH8vB1Ph5NLkwoWJIEWU0Mp00oWYrVJrC";
     if (!stripePromise) {
-      stripePromise = loadStripe(
-        process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-      );
+      stripePromise = loadStripe(stripe_publishable_key);
     }
     return stripePromise;
   };
