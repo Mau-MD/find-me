4986dfc7b1301c077e86cd7214b17dca0a293d6f
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-11-11 20:30:27 -0800

    Create FormPerdido.tsx

---
+++ src/pages/FormPerdido.tsx
@@ -0,0 +1,14 @@
+import { Center, Group } from "@mantine/core";
+import React from "react";
+
+const formPerdidos = () => {
+    return (
+        <>
+            <Group position="center">
+                <h1>Encuentra tu mascota</h1>
+            </Group>
+        </>
+    );
+};
+
+export default formPerdidos;
