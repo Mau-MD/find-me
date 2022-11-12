cdc02184fedbd5b8889d91ccf3695f86c987fa84
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-11-11 22:47:57 -0800

    Trying to make dark and light mode

--- src/components/perroPerdidoPerfil/Carousel.tsx
+++ src/components/perroPerdidoPerfil/Carousel.tsx
@@ -2,11 +2,13 @@ import { Carousel, Image } from '@mantine/carousel';
 
 function PerroCarousel() {
   return (
-    <Carousel sx={{ maxWidth: 320}} mx="auto" withIndicators height={200}>
-      <Carousel.Slide><Image
-      radius="md"
-      src="./helloimdude.jpg"
-      />
+    <Carousel sx={{ width: "50%" }} mx="auto" withIndicators height={200}>
+      <Carousel.Slide>
+        hola{/* <Image
+        radius="md"
+        src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+        alt="Random unsplash image"
+      /> */}
       </Carousel.Slide>
       <Carousel.Slide>2</Carousel.Slide>
       <Carousel.Slide>3</Carousel.Slide>
--- src/pages/PerroPerdidoPerfil.tsx
+++ src/pages/PerroPerdidoPerfil.tsx
@@ -4,27 +4,11 @@ import PerroCarousel from "../components/perroPerdidoPerfil/Carousel";
 
 const Profile = () => {
     return (
-        <>
+        <div>
             <Group position="center">
-                <Flex
-                direction={ "column" }
-                justify="center"
-                align="center">
-                    <h1>Nombre</h1>
-                    <div style={{ width: "100vh", height: "100vh"}}>
-                        <Flex
-                            gap="lg"
-                            justify="space-between"
-                            align="center"
-                            direction="row"
-                            wrap="wrap"
-                        >
-                            <PerroCarousel />
-                        </Flex>
-                    </div>
-                </Flex>
+                
             </Group>
-        </>
+        </div>
     )
 }
 
