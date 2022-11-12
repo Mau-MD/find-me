33ded430875cc5ed11b41666249c03c81cc0b3ce
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-11-11 22:37:13 -0800

    form vista perro & cambiar raza txtinpt por select

--- src/pages/busqueda.tsx
+++ src/pages/busqueda.tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from "next";
-import { Group, Text, Flex, TextInput, Textarea, NumberInput, useMantineTheme } from '@mantine/core'
+import { Group, Text, Flex, TextInput, Textarea, NumberInput, useMantineTheme, Checkbox, Select } from '@mantine/core'
 import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from '@mantine/dropzone';
 import { useRef } from "react";
 import { IconUpload, IconPhoto, IconX } from '@tabler/icons';
@@ -26,8 +26,16 @@ const busqueda: NextPage = () => {
         />
       </Flex>
       <Flex mb={12}>
-        <TextInput label="Raza" placeholder="ej. Husky" mr={12} style={{ width: "50%" }} />
-        <TextInput label="Nombre del dueño" placeholder="ej. Juan Pérez" style={{ width: "50%" }} />
+        <TextInput label="Nombre del dueño" placeholder="ej. Juan Pérez" style={{ width: "50%" }} mr={12} />
+        <Select
+          label="Raza"
+          placeholder="Pick one"
+          data={[
+            { value: 'husky', label: 'husky' },
+            { value: 'placeholder', label: 'placeholder' }
+          ]}
+          style={{ width: "50%" }}
+        />
       </Flex>
       <Flex mb={12}>
         <TextInput label="Color" placeholder="Blanco..." mr={12} style={{ width: "50%" }} />
@@ -36,42 +44,47 @@ const busqueda: NextPage = () => {
       <Textarea
         placeholder="Detalles particulares"
         label="Tus comentarios"
+        mb={18}
+      />
+      <Checkbox 
+        label="Ofrezco una recompensa por este perro"
         mb={26}
       />
-    <Dropzone
-      onDrop={(files) => console.log('accepted files', files)}
-      onReject={(files) => console.log('rejected files', files)}
-      maxSize={3 * 1024 ** 2}
-      accept={IMAGE_MIME_TYPE}
-    >
-      <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
-        <Dropzone.Accept>
-          <IconUpload
-            size={50}
-            stroke={1.5}
-          />
-        </Dropzone.Accept>
-        <Dropzone.Reject>
-          <IconX
-            size={50}
-            stroke={1.5}
-            color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
-          />
-        </Dropzone.Reject>
-        <Dropzone.Idle>
-          <IconPhoto size={50} stroke={1.5} />
-        </Dropzone.Idle>
 
-        <div>
-          <Text size="xl" inline>
-            Drag images here or click to select files
-          </Text>
-          <Text size="sm" color="dimmed" inline mt={7}>
-            Attach as many files as you like, each file should not exceed 5mb
-          </Text>
-        </div>
-      </Group>
-    </Dropzone>
+      <Dropzone
+        onDrop={(files) => console.log('accepted files', files)}
+        onReject={(files) => console.log('rejected files', files)}
+        maxSize={3 * 1024 ** 2}
+        accept={IMAGE_MIME_TYPE}
+      >
+        <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
+          <Dropzone.Accept>
+            <IconUpload
+              size={50}
+              stroke={1.5}
+            />
+          </Dropzone.Accept>
+          <Dropzone.Reject>
+            <IconX
+              size={50}
+              stroke={1.5}
+              color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
+            />
+          </Dropzone.Reject>
+          <Dropzone.Idle>
+            <IconPhoto size={50} stroke={1.5} />
+          </Dropzone.Idle>
+
+          <div>
+            <Text size="xl" inline>
+              Drag images here or click to select files
+            </Text>
+            <Text size="sm" color="dimmed" inline mt={7}>
+              Attach as many files as you like, each file should not exceed 5mb
+            </Text>
+          </div>
+        </Group>
+      </Dropzone>
     </div>
   )
 }
